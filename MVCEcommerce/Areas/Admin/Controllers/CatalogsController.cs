d18aff2e6511166971b4e80db16ecf9c23f29862
furkaninal0 <ileinal521@gmail.com>
2025-10-07 12:09:27 +0300

    catalog

--- MVCEcommerce/Areas/Admin/Controllers/CatalogsController.cs
+++ MVCEcommerce/Areas/Admin/Controllers/CatalogsController.cs
@@ -2,28 +2,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MVCECommerceData;
-using System.Threading.Tasks;
 
 namespace MVCEcommerce.Areas.Admin.Controllers;
+
+
 [Area("Admin")]
 [Authorize(Roles = "Administrators , ProductAdministrators")]
-public class CategoriesController(
+public class CatalogsController( 
     DbcontextEcommerce dbcontext
     ) : Controller
 {
     public async Task<IActionResult> Index()
     {
-        var items = await dbcontext.Categories.OrderBy(p => p.NameTr).ToListAsync();
+        var items = await dbcontext.Catalogs.OrderBy(p => p.NameTr).ToListAsync();
 
         return View(items);
     }
     public IActionResult Create()
     {
-        return View(new Category { });
+        return View(new Catalog { });
     }
 
     [HttpPost]
-    public async Task<IActionResult> Create(Category model)
+    public async Task<IActionResult> Create(Catalog model)
     {
         model.CreatedAt = DateTime.UtcNow;
 
@@ -33,14 +34,13 @@ public class CategoriesController(
     }
     public async Task<IActionResult> Edit(Guid id)
     {
-        var item = await dbcontext.Categories.FirstOrDefaultAsync(p => p.Id == id);
+        var item = await dbcontext.Catalogs.FirstOrDefaultAsync(p => p.Id == id);
         return View(item);
-    }
-
+    }   
     [HttpPost]
-    public async Task<IActionResult> Edit(Category model)
+    public async Task<IActionResult> Edit(Catalog model)
     {
-        var item = await dbcontext.Categories.SingleOrDefaultAsync(p => p.Id == model.Id);
+        var item = await dbcontext.Catalogs.SingleOrDefaultAsync(p => p.Id == model.Id);
 
         item.NameTr = model.NameTr;
         item.NameEn = model.NameEn;
@@ -48,17 +48,16 @@ public class CategoriesController(
 
         dbcontext.Update(item);
         await dbcontext.SaveChangesAsync();
-       
+
         return RedirectToAction(nameof(Index));
     }
     public async Task<IActionResult> Delete(Guid id)
     {
-        var item = await dbcontext.Categories.SingleOrDefaultAsync(p => p.Id == id);
+        var item = await dbcontext.Catalogs.SingleOrDefaultAsync(p => p.Id == id);
         dbcontext.Remove(item);
-        await dbcontext.SaveChangesAsync();  
+        await dbcontext.SaveChangesAsync();
         return RedirectToAction(nameof(Index));
     }
-
 
 
 }
